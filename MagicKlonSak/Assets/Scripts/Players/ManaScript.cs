c29a6c821d2a32ba53405f28238fdf49f80a4e0b
Baginbix <maus_tim@hotmail.com>
2014-06-30 20:07:36 +0100

    nytt shit

---
+++ MagicKlonSak/Assets/Scripts/Players/ManaScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManaScript : MonoBehaviour {
+	public float MaxMana;
+	public float CurrentMana;
+	public float ManaReg;
+
+	void Start()
+	{
+		SetDefaultManaReg();
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		RegenMana();
+	}
+
+	void SetDefaultManaReg()
+	{
+		ManaReg = (MaxMana*0.1f);
+	}
+
+	void RegenMana()
+	{
+		CurrentMana += ManaReg*Time.deltaTime;
+
+		if(CurrentMana > MaxMana)
+		{
+			CurrentMana = MaxMana;
+		}
+	}
+}
---
+++ MagicKlonSak/Assets/Scripts/Players/SummonMinions.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SummonMinions : MonoBehaviour {
+	public Transform prefab;
+	public float SummonRange;
+	Vector3 spawnPoint;
+	NavMeshAgent agent;
+	Vector3 defaultSpawnPoint = new Vector3(1000,1000,1000);
+
+	// Use this for initialization
+	void Start () {
+		spawnPoint = defaultSpawnPoint;
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			GetSpawnpoint();
+		}
+
+		if(spawnPoint != defaultSpawnPoint)
+		{
+			if(CheckRange() )
+			{
+				if(CheckMana())
+				{
+					Summon();
+				}
+			}
+			else
+			{
+				agent.enabled = true;
+				agent.SetDestination(spawnPoint);
+			}
+		}
+	}
+
+	bool CheckRange()
+	{
+		return Vector3.Distance(spawnPoint,transform.position)<=SummonRange;
+	}
+
+	void GetSpawnpoint()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(ray,out hit) && hit.transform.tag =="Mark" && CheckMana())
+		{
+			spawnPoint = hit.point;
+		}
+	}
+
+	bool CheckMana()
+	{
+		BaseUnit unit = prefab.GetComponent<BaseUnit>();
+		return unit.cost <= GetComponent<ManaScript>().CurrentMana;
+	}
+
+	public void Summon()
+	{
+		Instantiate(prefab, spawnPoint, transform.rotation);
+		GetComponent<ManaScript>().CurrentMana -= prefab.GetComponent<BaseUnit>().cost;
+		spawnPoint = defaultSpawnPoint;
+		agent.enabled = false;
+	}
+
+	public void CancelMovment()
+	{
+		agent.enabled = false;
+		spawnPoint = defaultSpawnPoint;
+	}
+}
