6388bf831ca68493e30b753458292017dd87e3a4
unknown <kalllez123@hotmail.se>
2014-08-29 21:22:22 +0200

    Added basic spell functionality! and all the meta-data apperantly

---
+++ MagicKlonSak/Assets/Scripts/Spells/Spell.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spell : MonoBehaviour {
+    
+    /// <summary>
+    /// In milliseconds
+    /// </summary>
+    public float Cooldown;
+    public int Manacost;
+    public KeyCode hotkey;
+    public GameObject SpellToCast;
+
+    private float cooldownRemaining;
+    private ManaScript ownerMana;
+
+	void Start() 
+    {
+        cooldownRemaining = 0;
+	}
+	
+	void Update() 
+    {
+        if (Input.GetKeyDown(hotkey) && CanCast())
+            Cast();
+	}
+
+    public void Cast()
+    {
+        ownerMana.CurrentMana -= Manacost;
+        GameObject spawnedSpell = (GameObject)Instantiate(SpellToCast);
+    }
+
+    protected bool CanCast()
+    {
+        return cooldownRemaining <= 0 && ownerMana.CurrentMana >= Manacost;
+    }
+
+    protected void HandleCooldown()
+    {
+        if (cooldownRemaining >= 0)
+            cooldownRemaining -= Time.deltaTime;
+    }
+}
