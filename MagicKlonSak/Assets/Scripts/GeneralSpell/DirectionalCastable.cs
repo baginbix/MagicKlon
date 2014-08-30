bb3c3712089b367b6980dafdf41891b367af0ed9
unknown <kalllez123@hotmail.se>
2014-08-30 08:19:09 +0200

    Directional spell + Interface for onSpellHit

---
+++ MagicKlonSak/Assets/Scripts/GeneralSpell/DirectionalCastable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    public class DirectionalCastable : MonoBehaviour
+    {
+        public Vector3 direction;
+        public float speed;
+        public ISpellHit spellHit;
+
+        void Start()
+        {
+            enabled = false;    
+        }
+
+        public void Activate(Vector3 direction, float speed)
+        {
+            enabled = true;
+            this.speed = speed;
+            this.direction = direction;
+        }
+
+        void Update()
+        {
+            transform.Translate(direction * speed);
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            spellHit.OnHitAction(collision.gameObject, gameObject, collision.contacts[0].point);
+        }
+
+    }
+}
---
+++ MagicKlonSak/Assets/Scripts/GeneralSpell/ISpellHit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    public interface ISpellHit
+    {
+        void OnHitAction(GameObject target, GameObject owner, Vector3 hitPoint, float damage = 0, float AoERange = 0);
+    }
+}
