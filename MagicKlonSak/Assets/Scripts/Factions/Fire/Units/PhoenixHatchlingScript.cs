5986848c460b1a15adb3a6afed461ecba2f1a5f9
Dalfen <fredrik.rodseth@gmail.com>
2014-10-05 14:59:50 +0200

    en till unit =O

---
+++ MagicKlonSak/Assets/Scripts/Factions/Fire/Units/PhoenixHatchlingScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoenixHatchlingScript : BaseFireUnit {
+    public Transform prefab;
+
+	// Use this for initialization
+	void Start () 
+    {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        base.Update();
+	}
+
+    public override void OnDeath()
+    {
+
+        MinionList minionListScript = GameObject.FindGameObjectWithTag("Player1").GetComponentInChildren<MinionList>();
+        Debug.Log(minionListScript.name);
+        minionListScript.minionList.Add((Transform)Instantiate(prefab, new Vector3(transform.position.x + 1, transform.position.y, transform.position.z), transform.rotation));
+
+    }
+}
