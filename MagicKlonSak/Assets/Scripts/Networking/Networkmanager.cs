426ae73db70648e0ff9261e28a2b59bda6e9c143
Baginbix <maus_tim@hotmail.com>
2014-10-12 18:43:45 +0100

    try 2!

--- MagicKlonSak/Assets/Scripts/Networking/Networkmanager.cs
+++ MagicKlonSak/Assets/Scripts/Networking/Networkmanager.cs
@@ -85,17 +85,12 @@ public class Networkmanager : MonoBehaviour
 			if (GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
 				StartServer();
 			
-			/*if (GUI.Button(new Rect(100, 250, 250, 100), "Refresh Hosts"))
-				RefreshHostList();
-			
-			if (hostList != null)
+			if (foundPartner != null)
 			{
-				for (int i = 0; i < hostList.Length; i++)
-				{
-					if (GUI.Button(new Rect(400, 100 + (110 * i), 300, 100), hostList[i].gameName))
-						JoinServer(hostList[i]);
-				}
-			}*/
+				if (GUI.Button(new Rect(400, 200, 300, 100), "Find Server"))
+					FindServer();
+			}
 		}
 	}
+
 }
