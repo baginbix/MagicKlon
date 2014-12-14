66c767bb4606328a18d3177a88db760b3cadf06a
Baginbix <maus_tim@hotmail.com>
2014-12-14 20:34:30 +0100

    buff system layout

---
+++ MagicKlonSak/Assets/Scripts/Minions/StatModifier.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum ModifierType
+{
+	Buff,
+	Debuff
+}
+
+public enum StatusEffects
+{
+	MovmentSpeed,
+	AttackDamage,
+	AttackSpeed,
+	Health
+}
+
+public struct StatusEffect
+{
+	public StatusEffects statusEffect; 
+	public float effect;
+}
+
+
+public class StatModifier : MonoBehaviour {
+	public ModifierType type;
+
+	public StatusEffect[] effectList;
+
+	public string name;
+	public string description;
+
+	public float duration;
+	public float timeRemaining;
+
+
+	// Use this for initialization
+	void Awake()
+	{
+		timeRemaining = duration;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
